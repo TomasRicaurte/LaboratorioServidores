f2cfab8f51ad4092dc27519077c65fb4f07947cf
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-09-05 20:32:48 -0500

    Inteto de solucion al error de jest

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const http = require ("http");
+const host = "localhost";
+const port = 8000;
+
+const requestListener = function (req, res) {
+    const url = new URL(req.url, `https://localohst:${port}/`);
+    if (url.pathname === "/user"){
+        const name = url.searchParams.get("name")
+        const email = url.searchParams.get("email")
+
+        const userObj = {
+            name: name,
+            email: email
+        }
+        const userJson = JSON.stringify(userObj)
+    res.setHeader("Content.Type", "application/json") 
+    res.writeHead(200);
+    res.end(userJson);
+    } 
+};
+
+const server = http.createServer(requestListener);
+module.exports = server;
+
